871f722645917358bd8b66d34cabad06b0243bca
connorakey <connorakey@proton.me>
2026-01-13 13:14:34 +1100

    feat: add ContentView and RecordingHUD for audio recording interface

---
+++ macos-app/openwhisper/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  openwhisper
+//
+//  Created by Connor on 13/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SettingsView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
